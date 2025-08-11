a86a43d109f275e59b6504637e8f1a26f4675c1b
Jun Park <pj2417@gmail.com>
2025-08-11 23:58:57 +0900

    broadcast done

---
+++ maelstrom-broadcast/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	var messages []float64 = []float64{}
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		if num, ok := body["message"].(float64); ok {
+			messages = append(messages, num)
+		}
+
+		return n.Reply(msg, map[string]string{"type": "broadcast_ok"})
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any = map[string]any{}
+		// Update the message type to return back.
+		body["type"] = "read_ok"
+		body["messages"] = messages
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		return n.Reply(msg, map[string]string{
+			"type": "topology_ok",
+		})
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
