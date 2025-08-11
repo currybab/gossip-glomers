f00646b7fc6e60a00f828f83421c206744763be4
Jun Park <pj2417@gmail.com>
2025-08-11 23:41:18 +0900

    unique ids done

---
+++ maelstrom-unique-ids/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"sync/atomic"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	var counter uint64 = 0
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "generate_ok"
+		body["id"] = fmt.Sprintf("%s-%d", msg.Dest, atomic.AddUint64(&counter, 1))
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
